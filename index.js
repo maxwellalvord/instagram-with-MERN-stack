5c8256798bda8885208251eb9589c309de3cbdad
Maxwell Alvord <maxwellalvord@gmail.com>
2023-02-01 12:26:44 -0800

    Update: add stock images and set up auth with JWT and bcrypt

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ import helmet from "helmet"
 import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
+import authRoutes from "./routes/auth.js"
 import { register } from "./controllers/auth.js"
 
 
@@ -39,6 +40,10 @@ const upload = multer({ storage });
 
 // ROUTES WITH FILES
 app.post("/auth/register", upload.single("picture"), register);
+
+// ROUTES
+app.use("/auth", authRoutes);
+app.use("/users", userRoutes);
 
 // Mongoose setup
 const PORT = process.env.PORT || 6001;
